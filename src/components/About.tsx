ed035d6390af4a17f4f3e8c8782458a11df60923
Rohith2408 <kumarrohith24081999@gmail.com>
2024-11-16 20:03:33 +0530

    update

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -10,9 +10,9 @@ const About=()=>{
                 <img className={styles.banner} src={banner}/>
                 <div className={styles.image_wrapper}><img src={image} className={styles.image}/></div>
                 <div className={styles.body_wrapper}>
-                    <p className={styles.title}>Unleash the fire of fortune</p>
+                    <p className={styles.title}>Where Crypto Meets Comedy!</p>
                     <p className={styles.text}>
-                    Blaze, the fiery demon dragon, soars through the skies of a volcanic underworld, igniting opportunities with the flames from his tail and ruling the crypto realm with a crown of power. Born from the dark lands of chaos, Blaze brings the heat to meme coins, promising blazing-fast transactions and unrivaled potential. Join the flight and let the fire fuel your fortune!
+                    Dintlo is a trending meme coin known for its playful branding and community-driven growth. It’s quickly gaining popularity through social media buzz and humor, making it a high-risk, high-reward option for adventurous investors.
                     </p>
                 </div>
             </div>
